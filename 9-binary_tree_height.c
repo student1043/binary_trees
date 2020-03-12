43d267c14dd8609475b308e121c5f085a78c7247
student1043 <1043@holbertonschool.com>
2020-03-12 19:25:33 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t leftdep, rightdep;
+size_t leftdep = 0, rightdep = 0;
 if (!tree)
 {
 return (0);
 }
 rightdep = binary_tree_height(tree->right) + 1;
 leftdep = binary_tree_height(tree->left) + 1;
-if (leftdep >= rightdep)
+if (leftdep > rightdep)
 {
 return (leftdep);
 }
