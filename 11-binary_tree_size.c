57f740faa0188471b0fd6bba9340c9ccdccd86a5
student1043 <1043@holbertonschool.com>
2020-03-12 19:59:19 +0100

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+if (!tree)
+{
+return (0);
+}
+else
+{
+return(size(tree->left) + 1 + size(tree->right));
+}
+}
