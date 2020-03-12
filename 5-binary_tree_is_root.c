b437799866010126099d6cf01735e40b242905df
student1043 <1043@holbertonschool.com>
2020-03-12 17:04:46 +0100

    task 5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - Entry point
+ * @node: node
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (!node)
+{
+return (0);
+}
+if (!node->left && !node->right)
+{
+return (1);
+}
+if (node->left)
+{
+binary_tree_is_leaf(node->left);
+}
+if (node->right)
+{
+binary_tree_is_leaf(node->right);
+}
+return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -39,4 +39,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif
