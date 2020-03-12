999f7466334414a4b64b648bb10e798d34536e45
student1043 <1043@holbertonschool.com>
2020-03-12 18:06:27 +0100

    Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+int leftdep, rightdep;
+if (!tree)
+{
+return (0);
+}
+else
+{
+leftdep = binary_tree_height(tree->left);
+rightdep = binary_tree_height(tree->right);
+if (leftdep > rightdep)
+{
+return (leftdep + 1);
+}
+else
+{
+return (rightdep + 1);
+}
+}
+return (leftdep);
+}
