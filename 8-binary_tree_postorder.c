05f94a17a6bec09d10b7c0d46193dd232556b84f
student1043 <1043@holbertonschool.com>
2020-03-12 18:48:09 +0100

    Task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Entry point
+ * @tree: tree
+ * @func: function
+ * Return: Always 0 (Success)
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (!tree || !func)
+{
+return;
+}
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
+}
