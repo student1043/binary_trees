e68cfdf05683655870a52620fedb0778763c5e41
student1043 <1043@holbertonschool.com>
2020-03-12 16:52:44 +0100

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: node
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (!node)
+{
+return (0);
+}
+if (node->left && node->right)
+{
+return (1);
+}
+else
+{
+return binary_tree_is_leaf(node->left) + binary_tree_is_leaf(node->right);
+}
+return;
+}
--- binary_trees.h
+++ binary_trees.h
@@ -37,4 +37,6 @@ void binary_tree_delete(binary_tree_t *tree);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
+int binary_tree_is_leaf(const binary_tree_t *node);
+
 #endif
