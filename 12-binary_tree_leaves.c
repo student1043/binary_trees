e1c86d53290f5674052f586c6e95cff105ecfec1
student1043 <1043@holbertonschool.com>
2020-03-13 18:09:26 +0100

    task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+if (!tree)
+{
+return (NULL);
+}
+if (!tree->left && !tree->right)
+{
+return (1);
+}
+else
+{
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+}
