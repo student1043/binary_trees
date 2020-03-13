7cae3ff7a7dffc90fe6d2fe78b3f3aae69263372
student1043 <1043@holbertonschool.com>
2020-03-13 19:17:43 +0100

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,7 +22,10 @@ return (0);
 }
 if ((tree->left) && (tree->right))
 {
-return (0);
+if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+{
+return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
 }
 return (0);
 }
