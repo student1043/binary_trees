d9b7935cc6ef6cf8740a0877cde5d9c1b53b9027
student1043 <1043@holbertonschool.com>
2020-03-13 18:52:52 +0100

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+int heightleft, heightright;
+if (!tree)
+{
+return (0);
+}
+if (!tree->left && !tree->right)
+{
+return (1);
+}
+heightleft = binary_tree_balance(tree->left);
+heightright = binary_tree_balance(tree->right);
+return (heightleft + heightright + 1);
+}
