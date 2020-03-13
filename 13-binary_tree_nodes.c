2cb3350e3bafe173a7a5d0d49bcefcf7a5bb2c5b
student1043 <1043@holbertonschool.com>
2020-03-13 18:18:05 +0100

    Update 13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+if (!tree)
+{
+return (0);
+}
+if (!tree->left && tree->right)
+return 1 + binary_tree_nodes(tree->right);
+else if (tree->left && !tree->right)
+{
+return 1 + binary_tree_nodes(tree->left);
+}
+}
