1e8571006d4687a9b5e7a82d5ac69e9bee3ae628
student1043 <1043@holbertonschool.com>
2020-03-13 19:12:31 +0100

    Create 16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Entry point
+ * @tree: tree
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+if (!tree)
+{
+return (0);
+}
+if (!tree->left && !tree->right)
+{
+return (1);
+}
+if (!tree->left || !tree->right)
+{
+return (0);
+}
+if ((tree->left) && (tree->right))
+{
+return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+}
+return (0);
+}
