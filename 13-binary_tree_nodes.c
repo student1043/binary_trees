0692a7baa4faf5755fb34652bd96239f95b7ea73
student1043 <1043@holbertonschool.com>
2020-03-13 18:37:42 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,5 +12,9 @@ if (!tree)
 {
 return (0);
 }
+if (!binary_tree_nodes(tree->left) || !binary_tree_nodes(tree->right))
+{
+return (0);
+}
 return 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 }
